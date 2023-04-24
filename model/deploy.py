00cb9183d7d08715775d1c86b542a44947a9e52c
dev-tyta <hardeykoryar7@gmail.com>
2023-04-24 09:33:32 +0100

    Adding a description and how-to in markdown

--- model/deploy.py
+++ model/deploy.py
@@ -5,7 +5,9 @@ import gradio as gr
 with gr.Blocks() as demo:
     the_ = "# Human Detector and Counter" \
            "Using Faster-RCNN model, the app detects the people present in your image. " \
-           "It returns the image of t  "
+           "It returns the image with a bounding box over each person and the total number of people" \
+           "## How to Use" \
+           "1. "
     gr.Markdown(the_)
     # choices = gr.Dropdown(["Image", "Video"],
     #                       label="What type of Object would you like to detect?", onchange=detection)
