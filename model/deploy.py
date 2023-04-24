fad23f6e14df360815ffb82ddbd39dd3f4da8574
dev-tyta <hardeykoryar7@gmail.com>
2023-04-24 08:25:20 +0100

    rewrite in session

--- model/deploy.py
+++ model/deploy.py
@@ -2,26 +2,9 @@ import gradio as gr
 from faster_rcnn import img_detect, video_detection
 
 
-inp_image = gr.Image(type="filepath")
-out_image = gr.Image()
-inp_video = gr.Video()
-out_video = gr.PlayableVideo
-
-
-def detection(choice):
-    if choice == "Image":
-        gr.update(input=[inp_image], output=out_image, function=img_detect)
-    elif choice == "Video":
-        gr.update(inputs=[inp_video], output=out_video, function=video_detection)
-    # elif choice == "Web-Cam":
-    #     inputs = [input1]
-    #     output = outputs
-    #     function = function1
-
-
 with gr.Blocks() as demo:
-    choices = gr.Dropdown(["Image", "Video", "Web-Cam"],
-                          label="What type of Object would you like to detect?", onchange=detection)
+    # choices = gr.Dropdown(["Image", "Video"],
+    #                       label="What type of Object would you like to detect?", onchange=detection)
 
 
 if __name__ == "__main__":
