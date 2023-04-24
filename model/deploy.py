1d5e7fa61178e1fa91364d70150e7d3967126482
dev-tyta <hardeykoryar7@gmail.com>
2023-04-24 08:39:32 +0100

    Adding markdown part to gradio code

--- model/deploy.py
+++ model/deploy.py
@@ -3,6 +3,9 @@ from faster_rcnn import img_detect, video_detection
 
 
 with gr.Blocks() as demo:
+    gr.Markdown("# Human Detector and Counter"
+                "This app helps you detect humans present in your image."
+                "Upload your choice of Image and the app loads the  ")
     # choices = gr.Dropdown(["Image", "Video"],
     #                       label="What type of Object would you like to detect?", onchange=detection)
 
