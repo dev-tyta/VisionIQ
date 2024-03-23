38ad39360b006c1277f41976fbb15b37a5eb23fc
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-03-23 15:35:59 +0100

    Addition

--- src/image_utils.py
+++ src/image_utils.py
@@ -12,7 +12,7 @@ class ImageUtils:
 
     def image_handling(self, image):
         image = cv2.imread(filename=image)  # reading image with cv2
-        image = cv2.resize(image, dsize=image_resize)  # resizig the image to standard resolution
+        image = cv2.resize(image, dsize=self.image_resize)  # resizig the image to standard resolution
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # changing color from BGR to RGB
 
         image = image.transpose((2, 0, 1))  # swapping the color channels from channels last to channels first 
@@ -21,10 +21,7 @@ class ImageUtils:
         image = image / 255.0  # scaling image from (0,255) to (0,1)
         image = torch.FloatTensor(image)  # changes the numpy array to a tensor
 
-        image = image.to(device)
+        image = image.to(self.device)
 
         return image  # return the image
     
-
-    def video_handling(video_path):
-         video = cv2.VideoCapture(filename=video_path)
