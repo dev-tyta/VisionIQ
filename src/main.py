a11c3457a016cd9fc51779665c8845a44626a829
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-03-27 15:54:04 +0100

    Fixing code

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 # making a test for the project
 from detections import Detections
 
-image_detection = Detections.image_detection()
 image_path = '../data/fam1.HEIC'
 
-output = image_detection(image_path)
-
-print(output)
+output =Detections.image_detection(image_path)
+counting = Detections.batch_people_count(output)
