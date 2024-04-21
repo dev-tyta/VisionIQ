441a84aa5b8330863021302e7eac8d5f4f8b4db9
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-04-21 21:06:22 +0100

    Creating Instance for YOLO

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 # making a test for the project
 from src.model.fasterrcnn_detections import Detections
+from src.model.yolo_detections import YoloDetections
 
 image_path = 'data/acc.jpg'
 faster_rcnn = Detections()
+yolo = YoloDetections
 
-output = faster_rcnn.image_detection(image_path)
-counting = faster_rcnn.people_count(output)
+output = yolo.image_detection(image_path)
+counting = yolo.people_count(output)
 
 print(f"[INFO] People in the image: {counting}")
