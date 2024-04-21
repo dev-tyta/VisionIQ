397802f81dedb6c18bcdb23fbb5b49dfe04e2914
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-04-21 21:02:09 +0100

    Testing out a new imaage

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 # making a test for the project
-from src.model.yolo_detections import YoloDetections
+from src.model.fasterrcnn_detections import Detections
 
-image_path = 'data/p.jpg'
-yolo = YoloDetections()
+image_path = 'data/acc.jpg'
+faster_rcnn = Detections()
 
-output = yolo.detect_with_yolo(image_path)
-counting = yolo.people_count(output)
+output = faster_rcnn.image_detection(image_path)
+counting = faster_rcnn.people_count(output)
 
 print(f"[INFO] People in the image: {counting}")
