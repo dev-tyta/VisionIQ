f3b4435a2db797a8dc08620c949e9d98bdf4b546
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-04-24 21:28:05 +0100

    Found a solution to loading LLAVA Model from huggingface

--- src/model/model_loader.py
+++ src/model/model_loader.py
@@ -1,7 +1,7 @@
 # importing libraries
 import torch
 from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2 as fastercnn_model
-from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
+from transformers import AutoProcessor, AutoModelForPreTraining
 from src.model.config import device
 
 
@@ -9,26 +9,22 @@ class ModelLoader:
     def __init__(self):
         self.device = device
         
-    def load_fastercnn(device):
+    def load_fastercnn(self):
         # loading Faster RCNN ResNet50 model
         model = fastercnn_model(pretrained=True, progress=True, pretrained_backbone=True)
-        model.to(device)  
+        model.to(self.device)  
         model.eval() # prints out the architecture of the model
         return model
     
-    def load_yolo(device, model_name= "yolov5s"):
+    def load_yolo(self, model_name= "yolov5s"):
         model = torch.hub.load("ultralytics/yolov5", model_name, pretrained=True, trust_repo=True)
-        model.to(device)
+        model.to(self.device)
         model.eval()
         return model 
     
     def load_llava(self):
-        llava_processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
-        llava_model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf",
-                                                                        torch_dtype=torch.float16,
-                                                                        low_cpu_mem_usage=True,
-                                                                        use_flash_attention_2=True
-                                                                        )
+        llava_processor = AutoProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
+        llava_model = AutoModelForPreTraining.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
         llava_model.to(self.device)
         llava_model.eval()
         return llava_processor, llava_model
