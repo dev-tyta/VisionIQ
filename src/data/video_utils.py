28cc93dce00947d841854ab1552e066b98241862
Testimony Adekoya <74291997+dev-tyta@users.noreply.github.com>
2024-05-03 14:46:09 +0000

    Missing Requirements.txt file: Sorted

--- src/data/video_utils.py
+++ src/data/video_utils.py
@@ -1,6 +1,7 @@
 import cv2
 from torchvision.transforms import functional as F
 
+
 class VideoUtils:
     def __init__(self):
         pass
@@ -9,7 +10,7 @@ class VideoUtils:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = F.to_tensor(frame)
         return frame
-    
+
     def process_video(self, video):
         frames = []
 
@@ -19,14 +20,14 @@ class VideoUtils:
                 break
             frame = self.process_frame(frame)
             frames.append(frame)
-        
+
         video.release()
         return frames
-    
+
     def create_frame_batches(self, frames, batch_size=16):
         """
         Splits the list of frames into smaller lists of frames (batches), each with a size up to `batch_size`.
-        
+
         :param frames: A list of video frames.
         :param batch_size: The maximum number of frames per batch.
         :return: A list of batches, where each batch is a list of frames.
@@ -34,4 +35,3 @@ class VideoUtils:
         # Split the frames list into batches of size `batch_size`
         for i in range(0, len(frames), batch_size):
             yield frames[i:i + batch_size]
-    
--- src/model/model_loader.py
+++ src/model/model_loader.py
@@ -8,26 +8,31 @@ from src.model.config import device
 class ModelLoader:
     def __init__(self):
         self.device = device
-        
+
     def load_fastercnn(self):
         # loading Faster RCNN ResNet50 model
-        model = fastercnn_model(pretrained=True, progress=True, pretrained_backbone=True)
-        model.to(self.device)  
-        model.eval() # prints out the architecture of the model
+        model = fastercnn_model(
+            pretrained=True, progress=True, pretrained_backbone=True)
+        model.to(self.device)
+        model.eval()  # prints out the architecture of the model
         return model
-    
-    def load_yolo(self, model_name= "yolov5s"):
-        model = torch.hub.load("ultralytics/yolov5", model_name, pretrained=True, trust_repo=True)
+
+    def load_yolo(self, model_name="yolov5s"):
+        model = torch.hub.load("ultralytics/yolov5",
+                               model_name, pretrained=True, trust_repo=True)
         model.to(self.device)
         model.eval()
-        return model 
-    
+        return model
+
     def load_llava(self):
-        llava_processor = AutoProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
-        llava_model = AutoModelForPreTraining.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
+        llava_processor = AutoProcessor.from_pretrained(
+            "llava-hf/llava-v1.6-mistral-7b-hf")
+        llava_model = AutoModelForPreTraining.from_pretrained(
+            "llava-hf/llava-v1.6-mistral-7b-hf")
         llava_model.to(self.device)
         llava_model.eval()
         return llava_processor, llava_model
+
 
 model = ModelLoader()
 model.load_llava()
