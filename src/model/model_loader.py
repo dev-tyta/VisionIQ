db694d65c0793a4bf55202d96041058067de7684
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-05-05 18:39:21 +0100

    Retrieving former codes; to start new model compression methods to be used in Colab

--- src/model/model_loader.py
+++ src/model/model_loader.py
@@ -7,30 +7,30 @@ class ModelLoader:
     def __init__(self):
         self.device = device
 
-    # def load_fastercnn(self):
-    #     try:
-    #         model = fastercnn_model(pretrained=True, progress=True, pretrained_backbone=True)
-    #         model.to(self.device)
-    #         model.eval()
-    #         return model
-    #     except Exception as e:
-    #         print(f"Failed to load Faster R-CNN model: {e}")
-    #         return None
+    def load_fastercnn(self):
+        try:
+            model = fastercnn_model(pretrained=True, progress=True, pretrained_backbone=True)
+            model.to(self.device)
+            model.eval()
+            return model
+        except Exception as e:
+            print(f"Failed to load Faster R-CNN model: {e}")
+            return None
 
-    # def load_yolo(self, model_name="yolov5s"):
-    #     try:
-    #         model = torch.hub.load("ultralytics/yolov5", model_name, pretrained=True)
-    #         model.to(self.device)
-    #         model.eval()
-    #         return model
-    #     except Exception as e:
-    #         print(f"Failed to load YOLO model: {e}")
-    #         return None
+    def load_yolo(self, model_name="yolov5s"):
+        try:
+            model = torch.hub.load("ultralytics/yolov5", model_name, pretrained=True)
+            model.to(self.device)
+            model.eval()
+            return model
+        except Exception as e:
+            print(f"Failed to load YOLO model: {e}")
+            return None
 
     def load_llava(self):
         try:
-            # llava_processor = AutoTokenizer.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", cache_dir=None)
-            # llava_model = AutoModelForPreTraining.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", cache_dir=None)
+            llava_processor = AutoTokenizer.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", cache_dir=None)
+            llava_model = AutoModelForPreTraining.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", cache_dir=None)
             llava_model = pipeline("feature-extraction", model="llava-hf/llava-v1.6-mistral-7b-hf", device=self.device)
             llava_model.to(self.device)
             llava_model.eval()
@@ -40,11 +40,11 @@ class ModelLoader:
             return None, None
 
 model = ModelLoader()
-# faster_rcnn_model = model.load_fastercnn()
-# yolo_model = model.load_yolo()
+faster_rcnn_model = model.load_fastercnn()
+yolo_model = model.load_yolo()
 llava_processor, llava_model = model.load_llava()
 
 # Use some print statements or logging to check the models loaded
-# print("Faster R-CNN Model Loaded:", faster_rcnn_model is not None)
-# print("YOLO Model Loaded:", yolo_model is not None)
+print("Faster R-CNN Model Loaded:", faster_rcnn_model is not None)
+print("YOLO Model Loaded:", yolo_model is not None)
 print("LLaVA Model and Processor Loaded:", llava_model is not None)
