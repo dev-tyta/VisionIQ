9b9569e342ef58d2ae5e1212d31ef9736548e1af
Testimony Adekoya <hardeykoryar7@gmail.com>
2025-01-02 21:12:21 +0100

    WIP: Removing the LLAVA model and going with the plain counting model for now

--- src/model_setup/model_loader.py
+++ src/model_setup/model_loader.py
@@ -28,23 +28,11 @@ class ModelLoader:
             print(f"Failed to load YOLO model: {e}")
             return None
 
-    def load_llava(self):
-        try:
-            llava_processor = AutoTokenizer.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", cache_dir=None)
-            llava_model = AutoModelForPreTraining.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", cache_dir=None)
-            llava_model.to(self.device)
-            llava_model.eval()
-            return llava_processor, llava_model
-        except Exception as e:
-            print(f"Failed to load LLaVA model: {e}")
-            return None, None
 
 model = ModelLoader()
 faster_rcnn_model = model.load_fastercnn()
 yolo_model = model.load_yolo()
-# llava_processor, llava_model = model.load_llava()
 
 # Use some print statements or logging to check the models loaded
 print("Faster R-CNN Model Loaded:", faster_rcnn_model is not None)
 print("YOLO Model Loaded:", yolo_model is not None)
-# print("LLaVA Model and Processor Loaded:", llava_model is not None)
